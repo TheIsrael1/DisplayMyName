3fe852c9cf5236a436180442fe45cd6e13b4f523
ehindero israel <iehindero419@stu.ui.edu.ng>
2021-08-18 04:24:30 +0100

    latest commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-document.write("MY NAME IS EHINDERO ISRAEL");
+
+
+function display (){
+
+    document.write("<h1>MY NAME IS EHINDERO ISRAEL</h1><br/><a href='https://epic-shaw-6cd75e.netlify.app/'>Resume</a>");
+
+};
+
